c1fe4d9bd77ae0e1f350f6a54ec3a49055bd852f
TeriyakiThames <64459916+TeriyakiThames@users.noreply.github.com>
2026-09-09 14:17:02 +0700

    chore: change package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
     namespace = "eu.kanade.tachiyomi"
 
     defaultConfig {
-        applicationId = "app.mihon"
+        applicationId = "com.thames.mihon_sync"
 
         versionCode = 29
         versionName = "0.20.4"
